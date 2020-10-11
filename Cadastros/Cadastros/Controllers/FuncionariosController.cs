76f76f3cb02811ae27daacf6d8dd675498a5faef
luwrs <l.willian16@gmail.com>
2020-10-11 04:47:46 -0300

    adicionando projeto cadastro de funcionarios MVC C#

---
+++ Cadastros/Cadastros/Controllers/FuncionariosController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Cadastros;
+
+namespace Cadastros.Controllers
+{
+    public class FuncionariosController : Controller
+    {
+        private MeuBanco db = new MeuBanco();
+
+        // GET: Funcionarios
+        public ActionResult Index()
+        {
+            return View(db.Funcionarios.ToList());
+        }
+
+        // GET: Funcionarios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Funcionarios funcionarios = db.Funcionarios.Find(id);
+            if (funcionarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(funcionarios);
+        }
+
+        // GET: Funcionarios/Create
+        public ActionResult Create()
+        {
+            var lista = db.Departamento.ToList();
+            ViewBag.ListaDep = lista;
+            return View();
+        }
+
+        // POST: Funcionarios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id_Func,Nome,Email,RG,CPF,Bairro,Cidade,UF,Departamento")] Funcionarios funcionarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Funcionarios.Add(funcionarios);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(funcionarios);
+        }
+
+        // GET: Funcionarios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Funcionarios funcionarios = db.Funcionarios.Find(id);
+            if (funcionarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(funcionarios);
+        }
+
+        // POST: Funcionarios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id_Func,Nome,Email,RG,CPF,Bairro,Cidade,UF,Departamento")] Funcionarios funcionarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(funcionarios).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(funcionarios);
+        }
+
+        // GET: Funcionarios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Funcionarios funcionarios = db.Funcionarios.Find(id);
+            if (funcionarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(funcionarios);
+        }
+
+        // POST: Funcionarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Funcionarios funcionarios = db.Funcionarios.Find(id);
+            db.Funcionarios.Remove(funcionarios);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
